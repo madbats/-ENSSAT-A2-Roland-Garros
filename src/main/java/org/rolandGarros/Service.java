038c4c293f2ebc9a408bedada44e630ed0d48052
mrochard <matthieu.rochard@etudiant.univ-rennes1.fr>
2022-05-03 14:57:06 +0200

    Ajout des Interfaces Dao et Service pour future implementation

---
+++ src/main/java/org/rolandGarros/Service.java
@@ -0,0 +1,17 @@
+package org.rolandGarros;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface Service<T> {
+	
+	// private Dao<T> dao;
+	
+	Collection<T> getAll();
+    int save(T t);
+    void update(T t);
+    void delete(T t);
+    Optional<T> get(int id);
+
+    // private void validate(T t);
+}
